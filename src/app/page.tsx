979dffe75012b37a5bc221063f9548f1b66d5608
calm-hoon <58981923+calm-hoon@users.noreply.github.com>
2026-05-04 18:19:20 +0900

    main text modify

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className="flex h-screen items-center justify-center">
-      <h1 className="text-4xl font-bold">silverway</h1>
+      <h1 className="text-4xl font-bold">silverway site</h1>
     </div>
   );
 }
